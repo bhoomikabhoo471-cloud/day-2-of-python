dc37c9705225a57512fcd27f0bc9b03414f0752a
bhoomikabhoo471-cloud <bhoomikabhoo471@gmail.com>
2025-11-04 23:43:57 +0530

    Add files via upload

---
+++ arithematic.py
@@ -0,0 +1,9 @@
+a=10
+b=20
+print("addiotion:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("Modulus:",a%b)
+print("Exponential:",a**b)
+print("floor division:",a//b)
---
+++ logical oprn.py
@@ -0,0 +1,5 @@
+a=10
+b=20
+print("And operator",a<b and a>b)
+print("or operator",a==b or a<b)
+print('Not operator',not a<b)
